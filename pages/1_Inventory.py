af938b8c0d097708890bd55f8118e169fabff897
Swooshoo <eliotlee1415@gmail.com>
2025-01-24 19:13:55 -0800

    Implemented a Dropdown Tutorial on Inventory.py. Enhances clarity for new users. Also implemented a Google Map link from directory.csv so that users can smoothly look up addresses.

--- pages/1_Inventory.py
+++ pages/1_Inventory.py
@@ -51,8 +51,8 @@ def load_response_data():
 
 def load_directory_data(directory_file_path):
     directory_data = pd.read_csv(
-        directory_file_path, skiprows=1, usecols = range(7),
-        names=["shelter_name", "city", "address", "email", "opening_hour", "closing_hour", "phone_number"]
+        directory_file_path, skiprows=1, usecols = range(8),
+        names=["shelter_name", "city", "address", "email", "opening_hour", "closing_hour", "phone_number", "map_url"]
     )
     return directory_data
 
@@ -147,12 +147,16 @@ def generate_weekly_df(shelter, response_df, today_date):
     })
     return weekly_df
 
-def render_shelter(shelter, city, address, email, opening_hour, closing_hour, phone_number, response_df, today_date):
+def render_shelter(shelter, city, address, email, opening_hour, closing_hour, phone_number, response_df, today_date, map_url):
     # Display the header with the divider color
     with st.container(border=True):
         st.subheader(shelter)
         #st.markdown(f"**:blue-background[{city}, CA] | {opening_hour} to {closing_hour}**")
-        st.markdown(f":gray-background[{city}, CA] | {opening_hour} to {closing_hour}")
+        col1, col2 = st.columns(2)
+        with col1:
+            st.link_button(f" :material/location_city: {city}", url=f"{map_url}", type = "primary")
+        with col2:
+            st.button(f"{opening_hour} -{closing_hour}", type="secondary")
         
         # Initialize an empty string for status message
         status_msg = ""
@@ -162,18 +166,11 @@ def render_shelter(shelter, city, address, email, opening_hour, closing_hour, ph
         #t.markdown(f"**{shelter} says:**")
         st.warning(f"{status_msg}",icon="📣")   
 
-        tab1, tab2, tab3 = st.tabs(["At A Glance", "Reach Us", "Supplies"])
+        tab1, tab2, tab3 = st.tabs(["**Overview**", "**Week**", "**Contact**"])
         
         with tab1:
             daily_priorities_and_excess(shelter, response_df,today_date)
-        # Tab 2: Display unit ability
         with tab2:
-            st.markdown(f"**Directory:**")
-            st.markdown(f"{address}")
-            st.markdown(f"{email}")
-            st.markdown(f"{phone_number}")
-
-        with tab3:
             weekly_df = generate_weekly_df(shelter, response_df, today_date)
             
             st.dataframe(
@@ -189,16 +186,91 @@ def render_shelter(shelter, city, address, email, opening_hour, closing_hour, ph
                         disabled=True,
                         format="%d",
                     ),
-                    "count_history": st.column_config.BarChartColumn(
-                        "Count (past 7 days)",
+                    "count_history": st.column_config.AreaChartColumn(
+                        "Count (Last 7 days)",
+                        width="small",
                         y_min=0, 
                         y_max=300,
                     ),
                 },
                 hide_index=True,
                 on_select="ignore",
-            ) 
+            )
+            
+        with tab3:
+            #st.markdown(f" :material/location_on: {address}")
+            st.markdown(f" :material/mail: {email}")
+            st.markdown(f" :material/call: {phone_number}")
 
+def dropdown_tutorial():
+    with st.popover(":material/help: Getting Started", use_container_width=True):
+        with st.container(border=True):
+            st.subheader("Site Name")
+            #st.markdown(f"**:blue-background[{city}, CA] | {opening_hour} to {closing_hour}**")
+            col1, col2 = st.columns(2)
+            with col1:
+                st.button(f" :material/location_city: City", type = "primary")
+                st.caption("Click the red button for a direct Google Maps link!")
+            with col2:
+                st.button("8:00 AM - 8:00 PM", type="secondary")
+                st.caption("Working hours of the site")
+            
+            # Initialize an empty string for status message
+            status_msg = "Daily Status Update"
+            st.warning(f"{status_msg}",icon="📣")
+            st.caption("The latest update from an on-site volunteer. Can be any specific messages, such as specific toiletries or parking instructions.")
+
+            tab1, tab2, tab3 = st.tabs(["Overview", "Week", "Contact"])
+            
+            with tab1:
+                st.caption("If you're looking to donate, an onsite volunteer has submitted what items they need or don't need here!")
+                st.success("Items that are in stock!", icon="✅")
+                st.error("Items that are running low!", icon="🚨")
+            
+            with tab2:
+                col1, col2 = st.columns([0.6,0.4])
+                with col1:
+                    
+                    df = pd.DataFrame(
+                        {
+                            "name": ["Water", "Food", "Clothes", "Hygiene Products", "Feminine Products", "Gift Cards"],
+                            "count": [random.randint(0, 1000) for _ in range(6)],
+                            "count_history": [[random.randint(0, 5000) for _ in range(7)] for _ in range(6)],
+                        }
+                    )
+
+                    st.dataframe(
+                        df,
+                        column_config={
+                            "name": st.column_config.TextColumn(
+                                "Item",
+                                disabled=True,
+                            ),
+                            "count": st.column_config.NumberColumn(
+                                "Count",
+                                help="Number of units of Item",
+                                disabled=True,
+                                format="%d",
+                            ),
+                            "count_history": st.column_config.AreaChartColumn(
+                                "Count (past 7 days)",
+                                y_min=0, 
+                                y_max=300,
+                            ),
+                        },
+                        hide_index=True,
+                        on_select="ignore",
+                ) 
+                with col2:
+                    st.caption("Count refers to the **latest number of items**. The Past 7 Days chart is to visualize a trend that donators can refer to as well!")
+                with tab3:
+                    contact, contact_caption = st.columns(2)
+                    with contact:
+                        st.markdown(f" :material/mail: Email")
+                        st.markdown(f" :material/call: Phone Number")
+                    with contact_caption:
+                        st.caption("Site-provided contact information!")
+                
 def main():
     # Load data
     response_data = load_response_data()
@@ -208,6 +280,9 @@ def main():
     st.info("For beta purposes, we have used placeholder data for shelters & our shelter directory!")
     
     st.header("📊 LAyudar - Inventory Tracker", divider = "gray")
+    
+    dropdown_tutorial()
+    
     cols_per_row = 2 # Number of cards per row
     for i in range(0, len(directory_data), cols_per_row):
         cols = st.columns(cols_per_row)  # Create columns for each row
@@ -224,7 +299,8 @@ def main():
                         closing_hour=row['closing_hour'],
                         phone_number=row['phone_number'],
                         response_df=response_data,
-                        today_date=today
+                        today_date=today,
+                        map_url=row['map_url']
                     )
 
 if __name__ == "__main__":
